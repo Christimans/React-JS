ec6b484ad5d1d51fdf9b754a4f2cf49188980cfa
Christimans <perezchristian3907@gmail.com>
2022-09-26 22:14:00 -0300

    ante ultima entrega

--- src/components/Cart/Checkout.jsx
+++ src/components/Cart/Checkout.jsx
@@ -1,74 +1,96 @@
 import { async } from "@firebase/util";
 import { addDoc, collection } from "firebase/firestore";
-import {db} from "../../Firebase/Firebase"
+import { db } from "../../Firebase/Firebase";
 import React from "react";
 import { useState } from "react";
 import { Form, Button } from "react-bootstrap";
 import { useCarritoContext } from "../Context/CarritoContexts";
+import { clear } from "@testing-library/user-event/dist/clear";
 
 export const Checkout = () => {
-  const [comprobanteid, setComprobanteid] =  useState()
+  const [comprobanteid, setComprobanteid] = useState();
 
-  const {itemCart, totalCart} = useCarritoContext ()
+  const { itemCart, totalCart, clear } = useCarritoContext();
 
-    const [buyer, setBuyer] = useState ({
-        Nombre:"",
-        Email:"",
-        Telefono:"",
-    })
+  const [buyer, setBuyer] = useState({
+    Nombre: "",
+    Email: "",
+    Telefono: "",
+  });
 
-    const {Nombre, Email, Telefono} = buyer
+  const { Nombre, Email, Telefono } = buyer;
 
-    const handleInputChange = (e) =>{
-        setBuyer(({ ...buyer,
-            [e.target.name]: e.target.value
-        }))
+  const handleInputChange = (e) => {
+    setBuyer({ ...buyer, [e.target.name]: e.target.value });
+  };
+  const generarOrden = async (comprobante) => {
+    try {
+      const col = collection(db, "ComprobantedeCompra");
+      const orden = await addDoc(col, comprobante);
+      console.log(orden);
+      setComprobanteid(orden.id);
+    } catch (error) {
+      console.error(error);
     }
-    const generarOrden = async (comprobante) =>{
-      try{
-        const col = collection(db,"ComprobantedeCompra")
-        const orden = await addDoc(col, comprobante)
-        console.log(orden)
-      }catch (error){
-        console.error(error)
-      }
-    }
+  };
 
+  const haldleSubmit = (e) => {
+    e.preventDefault();
+    const items = itemCart.map((e) => {
+      return {
+        id: e.id,
+        nombre: e.nombre,
+        precio: e.precio,
+        cantidad: e.cantidad,
+      };
+    });
+    const dia = new Date();
+    const pagototal = totalCart();
+    const comprobante = { buyer, items, dia, pagototal };
+    generarOrden(comprobante);
+    console.log(comprobante);
+    clear()
+  };
 
-    const haldleSubmit = (e) => {
-      e.preventDefault()
-      const items = itemCart.map(e=>{return {id:e.id, nombre:e.nombre, precio:e.precio, cantidad:e.cantidad}})
-      const dia = new Date()
-      const pagototal = totalCart()
-      const comprobante = {buyer, items, dia, pagototal}
-      generarOrden(comprobante)
-      console.log(comprobante)
-    }
-
-   
-
-  return (
+  return !comprobanteid ? (
     <div>
-        <h2>Finalizar Compra</h2>
-        <hr/>
-        <h3>Completa los siguientes datos</h3>
+      <h2>Finalizar Compra</h2>
+      <hr />
+      <h3>Completa los siguientes datos</h3>
       <Form onSubmit={haldleSubmit}>
-        <Form.Group className="mb-3" controlId="formBasicEmail" value={Nombre} onChange={handleInputChange}>
+        <Form.Group
+          className="mb-3"
+          controlId="formBasicEmail"
+          value={Nombre}
+          onChange={handleInputChange}
+        >
           <Form.Label>Nombre</Form.Label>
           <Form.Control type="text" placeholder="Nombre" />
         </Form.Group>
-        <Form.Group className="mb-3" controlId="formBasicEmail" value={Email} onChange={handleInputChange}>
+        <Form.Group
+          className="mb-3"
+          controlId="formBasicEmail"
+          value={Email}
+          onChange={handleInputChange}
+        >
           <Form.Label>Email</Form.Label>
-          <Form.Control type="email" placeholder="Enter email" />
+          <Form.Control type="email" placeholder="Enter email"  />
         </Form.Group>
-        <Form.Group className="mb-3" controlId="formBasicEmail" value={Telefono} onChange={handleInputChange}>
+        <Form.Group
+          className="mb-3"
+          controlId="formBasicEmail"
+          value={Telefono}
+          onChange={handleInputChange}
+        >
           <Form.Label>Telefono</Form.Label>
           <Form.Control type="number" placeholder="Telefono" />
         </Form.Group>
         <Button variant="primary" type="submit">
-            Enviar
+          Enviar
         </Button>
       </Form>
     </div>
+  ) : (
+    <h3> Su comprobante de compra es : {comprobanteid}</h3>
   );
 };
